8e6268e5746c3d4dd84f662ae569dd626df25bb9
knupfer <fknupfer@gmail.com>
2014-02-24 15:30:35 +0100

    Clean commented code

--- global-emacs.el
+++ global-emacs.el
@@ -1,19 +1,12 @@
-
 (defun get-string-from-file (filePath)
   "Return filePath's file content."
   (with-temp-buffer
     (insert-file-contents filePath)
     (buffer-string)))
 
-(defvar knu/foobar 1)
 (defvar knu/idle t)
 (when (not (file-exists-p "~/global-emacs"))
       (write-region "0" nil "~/global-emacs"))
-
-
-
-;(add-hook 'pre-command-hook '(lambda () (setq knu/idle nil)))
-;(add-hook 'post-command-hook '(lambda () (setq knu/idle t)))
 
 (add-hook 'pre-command-hook '(lambda () (when (equal (eval knu/idle) t)
                                      (setq knu/idle nil)
@@ -47,14 +40,3 @@
                                         1))
                                       nil "~/global-emacs")
                                      )))
-
-;(add-hook 'post-command-hook '(lambda () (append-to-file (number-to-string (eval knu/foobar)) nil "~/ubl")))
-
-
-;(add-hook 'post-command-hook '(lambda () (write-region 
-;                                     (number-to-string
-;                                      (+
-;                                       (string-to-number
-;                                        (get-string-from-file "~/global-emacs"))
-;                                       1))
-;                                     nil "~/global-emacs")))
