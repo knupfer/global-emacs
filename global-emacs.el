f08a59ac688085eecf966b31bf7303747bdb9621
knupfer <fknupfer@gmail.com>
2014-02-24 15:52:38 +0100

    Add copyright

--- global-emacs.el
+++ global-emacs.el
@@ -1,3 +1,38 @@
+;;; global-emacs.el --- Unify multiple emacs instances
+
+;; Copyright (C) 2014 Florian Knupfer
+
+;; Authors: Florian Knupfer <fknupfer@gmail.com>
+
+;; Version: 0.1
+;; Keywords: concurrent async
+;; X-URL: https://github.com/knupfer/global-emacs
+
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation; either version 3, or (at
+;; your option) any later version.
+
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+;; Boston, MA 02111-1307, USA.
+
+;;; Commentary:
+
+;; To use it, put this in your .emacs:
+;;
+;;     (require 'global-emacs)
+;;
+
+;;; Code:
+
+
 (defun get-string-from-file (filePath)
   "Return filePath's file content."
   (with-temp-buffer
@@ -51,3 +86,7 @@
                               'face 'dired-async-mode-message))
   (unless dired-async-mode
     (let ((visible-bell t)) (ding))))
+
+(provide 'global-emacs)
+
+;;; global-emacs.el ends here
