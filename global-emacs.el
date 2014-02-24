2396af707fdca1e6bfb62f518ae6480ff5a3c0f8
knupfer <fknupfer@gmail.com>
2014-02-24 15:07:28 +0100

    Make emacs count when not idle

--- global-emacs.el
+++ global-emacs.el
@@ -6,22 +6,35 @@
     (buffer-string)))
 
 (defvar knu/foobar 1)
-(defvar knu/idle t)
-;(run-with-idle-timer 1 t '(lambda () (setq knu/foobar (+ (eval knu/foobar) 1))))
+(defvar knu/idle nil)
+(when (not (file-exists-p "~/global-emacs"))
+      (write-region "0" nil "~/global-emacs"))
+
+
+
 ;(add-hook 'pre-command-hook '(lambda () (setq knu/idle nil)))
 ;(add-hook 'post-command-hook '(lambda () (setq knu/idle t)))
 
+(add-hook 'pre-command-hook '(lambda () (when (equal (eval knu/idle) t)
+                                     (setq knu/idle nil)
+                                     (write-region 
+                                      (number-to-string
+                                       (+
+                                        (string-to-number
+                                         (get-string-from-file "~/global-emacs"))
+                                        1))
+                                      nil "~/global-emacs")
+                                     )))
 
-;(add-hook 'pre-command-hook '(lambda () (setq knu/idle nil)))
+(run-with-idle-timer 2 t '(lambda () (setq knu/idle t)))
+
 ;(add-hook 'post-command-hook '(lambda () (append-to-file (number-to-string (eval knu/foobar)) nil "~/ubl")))
 
-(when (file-exists-p "~/global-emacs")
-      (write-region "0" nil "~/global-emacs"))
 
-(add-hook 'post-command-hook '(lambda () (write-region 
-                                     (number-to-string
-                                      (+
-                                       (string-to-number
-                                        (get-string-from-file "~/global-emacs"))
-                                       1))
-                                     nil "~/global-emacs")))
+;(add-hook 'post-command-hook '(lambda () (write-region 
+;                                     (number-to-string
+;                                      (+
+;                                       (string-to-number
+;                                        (get-string-from-file "~/global-emacs"))
+;                                       1))
+;                                     nil "~/global-emacs")))
